091f2ff547f5e71caacc11c551ddae1cab25c552
Jan H. Hansen <clonck.jh@googlemail.com>
2020-04-07 16:07:13 +0200

    Regstrierung der Cookies

--- src/Providers/GoogleTagManagerServiceProvider.php
+++ src/Providers/GoogleTagManagerServiceProvider.php
@@ -15,6 +15,22 @@ class GoogleTagManagerServiceProvider extends ServiceProvider
     */
     public function register()
     {
-       
+        /** @var ConsentRepositoryContract $consentRepository */
+        $consentRepository = pluginApp(ConsentRepositoryContract::class);
+        $consentRepository->registerConsent(
+            'tagManager',
+            'GoogleTagManager::GoogleTagManager.consentLabel',
+            [
+                'description' => 'GoogleTagManager::GoogleTagManager.consentDescription',
+                'provider' => 'GoogleTagManager::GoogleTagManager.consentProvider',
+                'lifespan' => 'GoogleTagManager::GoogleTagManager.consentLifespan',
+                'policyUrl' => 'GoogleTagManager::GoogleTagManager.consentPolicyUrl',
+                'group' => 'tracking',
+                'necessary' => false,
+                'isOptOut' => true,
+                'cookieNames' => ['_ga', '_gid', '_gat']
+            ]
+        );
     }
+    
 }
